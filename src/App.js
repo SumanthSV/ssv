b3a57c1593781c944d903bff439266a14f05624d
SumanthSV <sumanthsv04@gmail.com>
2024-07-11 22:29:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import About from './components/About';
+// import Contact from './components/Contact';
+import Experience from './components/Experience';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Projects from './components/Projects';
+import SocialLinks from './components/SocialLinks';
+import Certificates from './components/Certificates';
+
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Home/>
+      <SocialLinks/>
+      <About/>
+      <Projects/>
+      <Experience/>
+      <Certificates/>
+    </div>
+  );
+}
+
+export default App;
